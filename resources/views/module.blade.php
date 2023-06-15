8c8642b7945fa8efae7940bc6fc7cb1e025206eb
CrossJoy <87638792+CrossJoy@users.noreply.github.com>
2023-06-15 09:36:05 +0800

    update module hero sect

--- resources/views/module.blade.php
+++ resources/views/module.blade.php
@@ -2,19 +2,22 @@
 
 @section('content')
 
-<div id="hero" class="hero route bg-image" style="background-image: url(assets/img/hero-bg.jpg)">
+  <!-- ======= Hero Section ======= -->
+  <div id="hero" class="hero route bg-image" style="background-image: url(assets/img/hero-bg.jpg)">
     <div class="overlay-itro"></div>
     <div class="hero-content display-table">
       <div class="table-cell">
         <div class="container">
           <!--<p class="display-6 color-d">Hello, world!</p>-->
-          <h1 class="hero-title mb-4">I am Shahir</h1>
-          <p class="hero-subtitle"><span class="typed" data-typed-items="Designer, Developer, Freelancer, Photographer"></span></p>
+          <img src="/assets/img/logo.png" alt="Logo" width="150"  style="border-radius: 50%;">
+          <h1 class="hero-title mb-4">Module</h1>
+          <!-- <p class="hero-subtitle"><span class="typed" data-typed-items="Purify Yourself, Preach Others"></span></p> -->
           <!-- <p class="pt-3"><a class="btn btn-primary btn js-scroll px-4" href="#about" role="button">Learn More</a></p> -->
         </div>
       </div>
     </div>
-  </div>
+  </div><!-- End Hero Section -->
+
 
 
 <section id="contact" class="paralax-mf footer-paralax bg-image sect-mt4 route" style="background-image: url(assets/img/overlay-bg.jpg)">
